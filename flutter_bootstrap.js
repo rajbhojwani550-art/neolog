fb9edba80b9d2f492137c4a1378b94052e73edb3
neolog <rajbhojwani550@gmail.com>
2026-07-02 00:19:54 +0530

    deploy: calendar split layout fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4274024855"
+    serviceWorkerVersion: "2835275612"
   }
 });
